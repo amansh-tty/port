f1c098124e442a39e503102e8dc7a4a6160a6816
carnel <amanshetty300@gmail.com>
2025-02-25 12:07:16 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "portfolio",
+		name: "Aman Shetty.",
+		email: "amanshetty300@gmail.com",
+		logo: "https://i.pinimg.com/736x/0a/bd/66/0abd66d386429d31d317936567fce608.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Hello World!!!,  I'm AMAN",
+		description:
+			"I am a full stack developer from Bangalore.",
+	},
+
+	// about: {
+	// 	title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+	// 	description:
+	// 		"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	// },
+
+	// articles: {
+	// 	title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+	// 	description:
+	// 		"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	// },
+
+	projects: [
+		{
+			title: "EventBox",
+			description:
+				"This is a platform where students can register for the college events and and never miss out on any events.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/amansh-tty/van",
+		},
+
+		{
+			title: "Tinder for fashion",
+			description:
+				"A project where user can swipe right to like the product and swipe left to dislike the product and be able to view it on cart and which will lead to myntra landing page for that product",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/amansh-tty/Tinder-fashion",
+		},
+
+		{
+			title: "Finance Tracker",
+			description:
+				"This is project where user can login the financial data and track where the money is spent onto purchases",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/amansh-tty/Finance-Tracker",
+		},
+
+		{
+			title: "CSV-Analyzer",
+			description:
+				"This project analyzes your csv data and helps you visualize it in the graph format.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/amansh-tty/CSV-Data-Analyzer",
+		},
+
+		{
+			title: "Sorting Algorithm",
+			description:
+				"This was created using python module called pygame where you select an algorithm and it'll animate in the order of that algorithm",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/amansh-tty/Sorting-Algorithm-Visualizer",
+		},
+	],
+};
+
+export default INFO;
